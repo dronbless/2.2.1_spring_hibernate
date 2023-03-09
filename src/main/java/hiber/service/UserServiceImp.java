e93c41f9b7602abdaae5cb8acbee5599dbb2e5e7
Andrey Rumyantsev <119076724+dronbless@users.noreply.github.com>
2023-03-10 01:25:06 +0300

    withouttran

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -2,7 +2,6 @@ package hiber.service;
 
 import hiber.dao.UserDao;
 import hiber.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,7 +27,7 @@ public class UserServiceImp implements UserService {
    public List<User> listUsers() {
       return userDao.listUsers();
    }
-   @Transactional
+   @Transactional(readOnly = true)
    @Override
    public User getUserWithCar(String model, int series) {
       return userDao.getUserWithCar(model, series);
